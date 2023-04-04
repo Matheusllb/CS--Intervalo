6a5581f9fdc18e3a4a3b270c441e2c63c159cc41
Matheusllb <matheusluislopesbatista@gmail.com>
2023-04-04 20:22:05 -0300

    Diz se valores testados estão no ntervalo [10,20]

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CsE19
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //EXERCÍCIO 19
+            Console.Write("Verificando se valores informados estão no intervalo [10,20]\n\nQuantos valores deseja verificar? ");
+            int N = int.Parse(Console.ReadLine());
+            int v, vIn = 0, vOut = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.Write($"Valor ({i}): ");
+                v = int.Parse(Console.ReadLine());
+
+                if (v >= 10 && v <= 20)
+                    vIn++;
+                else vOut++;
+            }
+            Console.WriteLine($"\nIn: {vIn}");
+            Console.WriteLine($"Out: {vOut}");
+            Console.WriteLine("\nFim do Programa!\n");
+        }
+    }
+}
